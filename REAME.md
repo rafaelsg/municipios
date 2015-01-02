7584dae6667840930d91c3e95a710bebcfc8a95a
silveiragomesr <silveiragomesr@hotmail.com>
2015-01-01 23:29:09 -0200

    Subindo scripts de municipios e estados

---
+++ REAME.md
@@ -0,0 +1,5 @@
+Scripts de tabelas simples do PostgreSQL:
+
+Contem nomes das 5570 cidades e dos 27 estados do Brasil.
+
+A informação foi extraída do IBGE com data de atualização em julho/2014.
---
+++ estados.sql
@@ -0,0 +1,37 @@
+﻿CREATE TABLE estado (
+  codEstado SERIAL NOT NULL,
+  nome VARCHAR(30) NOT NULL,
+  uf CHAR(2) NOT NULL,
+
+  CONSTRAINT pk_estado
+    PRIMARY KEY (codEstado)
+    );
+
+INSERT INTO estado (nome, uf) VALUES
+('ACRE', 'AC'),
+('ALAGOAS', 'AL'),
+('AMAZONAS', 'AM'),
+('AMAPÁ', 'AP'),
+('BAHIA', 'BA'),
+('CEARÁ', 'CE'),
+('DISTRITO FEDERAL', 'DF'),
+('ESPÍRITO SANTO', 'ES'),
+('GOIÁS', 'GO'),
+('MARANHÃO', 'MA'),
+('MINAS GERAIS', 'MG'),
+('MATO GROSSO DO SUL', 'MS'),
+('MATO GROSSO', 'MT'),
+('PARÁ', 'PA'),
+('PARAÍBA', 'PB'),
+('PERNAMBUCO', 'PE'),
+('PIAUÍ', 'PI'),
+('PARANÁ', 'PR'),
+('RIO DE JANEIRO', 'RJ'),
+('RIO GRANDE DO NORTE', 'RN'),
+('RONDÔNIA', 'RO'),
+('RORAIMA', 'RR'),
+('RIO GRANDE DO SUL', 'RS'),
+('SANTA CATARINA', 'SC'),
+('SERGIPE', 'SE'),
+('SÃO PAULO', 'SP'),
+('TOCANTINS', 'TO');
